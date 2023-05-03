c3cb59fa0115494df2f382d3cfa9b3922490a89e
lumaghg <rotkehlchen567@gmail.com>
2023-05-03 19:39:17 +0200

    add plots

--- prediction/scripts/5_dataset_analysis.py
+++ prediction/scripts/5_dataset_analysis.py
@@ -3,6 +3,8 @@ from pandas import read_csv
 
 dataset = read_csv('../datasets/accidents_raw.csv')
 
+print(dataset.corr())
+
 print("accidents distributions")
 fig, ax = plt.subplots()
 ax.set_xlabel('Anzahl der Unfälle')
@@ -11,22 +13,18 @@ dataset['accidents'].value_counts().plot.bar()
 plt.show()
 
 
-# print('grouped plots')
-#
-# for column in ['hour', 'day', 'month']:
-#
-#    dataset.boxplot(column='accidents', by=column)
-#    dataset_grouped_by_hour = dataset.groupby([column]).mean().reset_index()
-#
-#    x = dataset_grouped_by_hour[column]
-#    y = dataset_grouped_by_hour['accidents']
-#
-#    fig, ax = plt.subplots()
-#    ax.set_xlabel(column)
-#    ax.set_ylabel('accidents')
-#    ax.scatter(x, y)
-#    ax.plot(x, y)
-#    plt.show()
+print('grouped plots')
+for column in ['hour', 'day', 'month']:
+    dataset.boxplot(column='accidents', by=column)
+    dataset_grouped_by_hour = dataset.groupby([column]).mean().reset_index()
+    x = dataset_grouped_by_hour[column]
+    y = dataset_grouped_by_hour['accidents']
+    fig, ax = plt.subplots()
+    ax.set_xlabel(column)
+    ax.set_ylabel('accidents')
+    ax.scatter(x, y)
+    ax.plot(x, y)
+    plt.show()
 
 
 print('ungrouped plots')
